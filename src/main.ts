629b86f6e01bf1765fc4a82120617543507c6700
Alan Hernandez <alan.herjuarez97@gmail.com>
2024-11-18 12:26:37 -0600

    added dockerfile and config ms

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers,
+    },
+  }, {
+    inheritAppConfig: true
+  })
+
+  await app.startAllMicroservices()
+
   await app.listen(3000);
 }
 bootstrap();
